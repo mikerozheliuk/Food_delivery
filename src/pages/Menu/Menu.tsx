0bf4da07706e0899cd6b0142cd3c08c2b04a2467
Mykhailo <98221429+mikerozheliuk@users.noreply.github.com>
2024-02-13 12:01:05 +0200

    add menulist (#8)

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,7 +2,6 @@ import { PREFIX } from "../../helpers/API";
 
 import Search from "../../components/Search/Search";
 import Headling from "../../components/Headling/Headling";
-import ProductCart from "../../components/ProductCart/ProductCart";
 
 import { Product } from "../../interfaces/product.interface";
 
@@ -10,6 +9,7 @@ import styles from "./Menu.module.scss";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Spinner } from "../../components/Spinner/Spinner";
+import { MenuList } from "./MenuList/MenuList";
 
 export function Menu() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -20,11 +20,6 @@ export function Menu() {
   const getMenu = async () => {
     try {
       setIsLoading(true);
-      await new Promise<void>((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 2000);
-      });
 
       const { data } = await axios.get<Product[]>(
         `${PREFIX}/products`
@@ -50,21 +45,12 @@ export function Menu() {
         <Headling>Menu</Headling>
         <Search placeholder="Введіть страву чи склад" />
       </div>
-      <div className={styles.menu}>
-        {!isLoading &&
-          products.map((p) => (
-            <ProductCart
-              key={p.id}
-              id={p.id}
-              name={p.name}
-              description={p.ingredients.join(", ")}
-              rating={p.rating}
-              price={p.price}
-              image={p.image}
-            />
-          ))}
+      <div className={styles.menu_list}>
+        {!isLoading && <MenuList products={products} />}
         {isLoading && <Spinner />}
       </div>
     </>
   );
 }
+
+export default Menu;
