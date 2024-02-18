f7e28d1d2f61d3c8f824ea9d6285ad17f6ec03fe
mike <rozheluikmisha@gmail.com>
2024-02-18 21:35:25 +0200

    create register and update user profile

--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -6,10 +6,15 @@ import {
 import axios, { AxiosError } from "axios";
 
 import { loadState } from "./storage";
+
 import { PREFIX } from "../helpers/API";
 
 export const JWT_PERSISTENT_STATE = "userData";
+
 import { LoginResponse } from "../interfaces/auth.interface";
+import { Profile } from "../interfaces/user.interface";
+
+import { RootState } from "./store";
 
 export interface UserPersistentState {
   jwt: string | null;
@@ -18,6 +23,8 @@ export interface UserPersistentState {
 export interface UserState {
   jwt: string | null;
   loginErrorMessage?: string;
+  registerErrorMessage?: string;
+  profile?: Profile;
 }
 
 const initialState: UserState = {
@@ -46,6 +53,48 @@ export const login = createAsyncThunk(
   }
 );
 
+export const register = createAsyncThunk(
+  "user/register",
+  async (params: {
+    email: string;
+    password: string;
+    name: string;
+  }) => {
+    try {
+      const { data } = await axios.post<LoginResponse>(
+        `${PREFIX}/auth/register`,
+        {
+          email: params.email,
+          password: params.password,
+          name: params.name,
+        }
+      );
+      return data;
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        throw new Error(e.response?.data.message);
+      }
+    }
+  }
+);
+
+export const getProfile = createAsyncThunk<
+  Profile,
+  void,
+  { state: RootState }
+>("user/getProfile", async (_, thunkApi) => {
+  const jwt = thunkApi.getState().user.jwt;
+  const { data } = await axios.get<Profile>(
+    `${PREFIX}/user/profile`,
+    {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    }
+  );
+  return data;
+});
+
 export const userSlice = createSlice({
   name: "user",
   initialState,
@@ -56,16 +105,38 @@ export const userSlice = createSlice({
     clearLoginError: (state) => {
       state.loginErrorMessage = undefined;
     },
+    clearRegisterError: (state) => {
+      state.registerErrorMessage = undefined;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(login.fulfilled, (state, action) => {
       if (!action.payload) {
         return;
       }
+
       state.jwt = action.payload.access_token;
     });
+
     builder.addCase(login.rejected, (state, action) => {
       state.loginErrorMessage = action.error.message;
+    });
+
+    builder.addCase(
+      getProfile.fulfilled,
+      (state, action) => {
+        state.profile = action.payload;
+      }
+    );
+
+    builder.addCase(register.fulfilled, (state, action) => {
+      if (!action.payload) {
+        return;
+      }
+      state.jwt = action.payload.access_token;
+    });
+    builder.addCase(register.rejected, (state, action) => {
+      state.registerErrorMessage = action.error.message;
     });
   },
 });
