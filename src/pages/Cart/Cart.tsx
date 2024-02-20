4d0ee74050053d499ac15fc153aef8e0b632c3ba
mike <rozheluikmisha@gmail.com>
2024-02-20 14:44:30 +0200

    create basket of goods and styles

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,80 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import axios from "axios";
+
 import Headling from "../../components/Headling/Headling";
+import CartItem from "../../components/CartItem/CartItem";
+
+import { Product } from "../../interfaces/product.interface";
+
+import { RootState } from "../../store/store";
+
+import { PREFIX } from "../../helpers/API";
+
+import styles from "./Cart.module.scss";
+
+const DELIVERY_FEE = 169;
 
 export function Cart() {
+  const [cartProducts, setCardProducts] = useState<Product[]>([]);
+
+  const items = useSelector((s: RootState) => s.cart.items);
+
+  const total = items
+    .map((i) => {
+      const product = cartProducts.find((p) => p.id === i.id);
+      if (!product) {
+        return 0;
+      }
+      return i.count * product.price;
+    })
+    .reduce((acc, i) => (acc += i), 0);
+
+  const getItem = async (id: number) => {
+    const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`);
+    return data;
+  };
+
+  const loadAllItems = async () => {
+    const res = await Promise.all(items.map((i) => getItem(i.id)));
+    setCardProducts(res);
+  };
+
+  useEffect(() => {
+    loadAllItems();
+  }, [items]);
+
   return (
     <>
-      <>
-        <Headling>Cart</Headling>
-      </>
+      <Headling className={styles.headling}>Корзина</Headling>
+      {items.map((i) => {
+        const product = cartProducts.find((p) => p.id === i.id);
+        if (!product) {
+          return;
+        }
+        return <CartItem key={product.id} count={i.count} {...product} />;
+      })}
+      <div className={styles.line}>
+        <div className={styles.text}>Всього</div>
+        <div className={styles.price}>
+          {total}&nbsp;<span>$</span>
+        </div>
+      </div>
+      <hr className={styles.hr} />
+      <div className={styles.line}>
+        <div className={styles.text}>Доставка</div>
+        <div className={styles.price}>
+          {DELIVERY_FEE}&nbsp;<span>$</span>
+        </div>
+      </div>
+      <hr className={styles.hr} />
+      <div className={styles.line}>
+        <div className={styles.text}>Всього {items.length} блюда</div>
+        <div className={styles.price}>
+          {total + DELIVERY_FEE}&nbsp;<span>$</span>
+        </div>
+      </div>
     </>
   );
 }
